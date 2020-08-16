eb11aaa59cdd2b4c925c11ab88cc049388e40fba
Naveen Chaudhary <cnaveen052@gmail.com>
2020-08-16 13:18:55 +0530

    Binary Search

---
+++ binarySearch.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include <vector>
+using namespace std;
+
+//https://leetcode.com/problems/first-bad-version/
+
+bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int lo=1, hi=n;
+        while(lo<hi){
+            int mid=lo+(hi-lo)/2;
+            if(isBadVersion(mid)) hi=mid;
+            else lo=mid+1;
+        }
+        return lo;
+    }
+};
