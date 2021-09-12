6c0e69515dd2dad2e3e6ffe4c366233886ea2ae3
Naveen Chaudhary <cnaveen052@users.noreply.github.com>
2021-09-12 19:06:14 +0530

    Update dijkstra.cpp

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -3,31 +3,33 @@
 #include <queue>
 using namespace std;
 
-typedef pair<int, int> ii;
+/*
+https://leetcode.com/problems/path-with-maximum-probability
+*/
 
 class Solution {
 public:
-    int minDistaceFromSToT(int n, vector<vector<int>>& edges, vector<int>& dist, int s, int t) {
-        vector<vector<ii>> g(n);
-        vector<int> d(n);
-        for(int k=0; k<edges.size(); k++){
-            const auto& e = edges[k];
-            int x = e[0], y = e[1];
-            int z = dist[k];
-            g[x].push_back({y, z});
-            g[y].push_back({x, z});
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& P, int s, int t) {
+        vector<vector<pair<int, double>>> g(n);
+        vector<int> seen(n);
+        vector<double> d(n, 0); d[s]=1;
+        for(int i=0;i<edges.size();i++){
+            int x=edges[i][0], y=edges[i][1];
+            g[x].push_back({y, P[i]});
+            g[y].push_back({x, P[i]});
         }
-        priority_queue<ii, vector<ii>, greater<ii>> q;
-        fill(d.begin(), d.end(), INT_MAX);
-        d[s] = 0;
+        priority_queue<pair<double, int>> q;
         q.push({d[s], s});
-        while (!q.empty()) {
-            auto [dd, u]=q.top(); q.pop();
-            if (dd != d[u]) continue;
-            for (auto& [v, w] : g[u]) {
-                if (d[v] > d[u] + w) {
-                    d[v] = d[u] + w;
-                    q.push({d[v], v});
+        while(q.size()){
+            auto [dis, x] = q.top(); q.pop();
+            if(seen[x]) continue;
+            seen[x]=1;
+            for(auto i:g[x]){
+                auto y=i.first;
+                auto w=i.second;
+                if(d[y]<dis*w){
+                    d[y]=dis*w;
+                    q.push({d[y], y});
                 }
             }
         }
