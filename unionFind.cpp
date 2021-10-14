7369d48645f6262cd2a044d431052d33b56e3c97
Naveen Chaudhary <cnaveen052@users.noreply.github.com>
2021-10-14 13:24:49 +0530

    Update unionFind.cpp

--- unionFind.cpp
+++ unionFind.cpp
@@ -9,10 +9,19 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> parent;
+    vector<int> parent, rank;
     
     void Union(int x, int y){
+        int xroot=find(x), yroot=find(y);
+        if(xroot!=yroot){
+            if(rank[xroot]<rank[yroot]) swap(xroot,yroot);
+            parent[yroot]=xroot;
+            if(rank[xroot]==rank[yroot]) rank[xroot]++;
+        }
+        /*
+        one liner without rank:-
         parent[find(y)]=parent[find(x)];
+        */
     }
     
     int find(int x){
@@ -23,7 +32,7 @@ public:
     int largestComponentSize(vector<int>& a) {
         int n=a.size();
         int mx=*max_element(a.begin(),a.end());
-        parent.resize(mx+1);
+        parent.resize(mx+1); rank.resize(mx+1);
         for(int i=0;i<parent.size();i++) parent[i]=i;
         for(auto i:a){
             for(int k=2;k*k<=i;k++){
